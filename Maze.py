a3c81506c9ca65c7c05d3b5960163a0c83f5abc4
raviblokhra <rb1012@scarletmail.rutgers.edu>
2021-02-19 18:43:46 -0500

    minor fixes

--- Maze.py
+++ Maze.py
@@ -119,7 +119,7 @@ class Maze:
         # self.advance_fire_one_step()
         self.display_maze()
         """
-        density_vs_nodes = pd.DataFrame(columns=('Density', 'Success Rate'))
+        density_vs_nodes = pd.DataFrame(columns=('Ignition Rate', 'Success Rate'))
         total_nodes = 0
         i = 0
         attempts = 0
@@ -134,7 +134,7 @@ class Maze:
                 self.maze = np.random.choice(
                     a=[0, 1],
                     size=(dim, dim),
-                    p=[1 - .30, .30])
+                    p=[1 - .10, .10])
                 self.maze[0][0] = 0
                 self.maze[dim - 1][dim - 1] = 0
 
@@ -144,10 +144,10 @@ class Maze:
                             if random.uniform(0, 1) <= self.q:  # q is the fire spread rate chosen by user
                                 self.maze[x][y] = 2
                 try:
-                    path, nodes_visited = self.a_star(start, end)
-                    # for x, y in path:
-                    # self.maze[x][y] = 3
-                    # self.display_maze()
+                    path, nodes_visited = self.bfs(start, end)
+                    """for x, y in path:
+                        self.maze[x][y] = 3
+                    self.display_maze()"""
                     if attempts == -1:
                         print(
                             f"Test {n_trials} has failed multiple times at current ignition rate {self.q} and will "
@@ -159,7 +159,7 @@ class Maze:
                         print("No path found, rerunning test")
                         attempts += 1
                         print(
-                            f"Current trial:  {successful_attempts} ,Attempt:  {attempts}  at {self.q} density")
+                            f"Current trial:  {successful_attempts} ,Attempt:  {attempts}  at {self.q} Ignition rate")
                         continue
                 except ZeroDivisionError:
                     print("An error has occurred, rerunning test")
@@ -167,22 +167,27 @@ class Maze:
                     continue
                 total_nodes += nodes_visited
                 # attempts = 0
-                # for x, y in path:
-                   # self.maze[x][y] = 3
-                # self.display_maze()
-                print(self.maze)
+
                 successful_attempts += 1
                 attempts += 1
                 print("Ignition rate", self.q)
                 print("Nodes visited", nodes_visited)
                 n_trials += 1
-
+            #for x, y in path:
+                #self.maze[x][y] = 3
+            #self.display_maze()
+            #print(self.maze)
             success_rate = successful_attempts / attempts
             density_vs_nodes.loc[i] = [self.q, success_rate]
-            density_vs_nodes.to_csv("astar_fire_success.csv", mode='a', index=False)
+            density_vs_nodes.to_csv("bfs_fire_success.csv", mode='a', index=False)
             i += 1
+        print(density_vs_nodes.head())
+        density_vs_nodes.plot(x="Ignition Rate", y="Success Rate")
+        plt.show()
 
-        """for density in np.arange(0.3, 1, 0.1):
+
+        """
+        for density in np.arange(0.0, 1, 0.1):
             total_nodes = 0
             successful_attempts = 0
             attempts = 0
@@ -195,7 +200,7 @@ class Maze:
                 self.maze[0][0] = 0
                 self.maze[dim - 1][dim - 1] = 0
                 try:
-                    path, nodes_visited = self.dfs(start, end)
+                    path, nodes_visited = self.bfs(start, end)
                     # for x, y in path:
                     # self.maze[x][y] = 3
                     # self.display_maze()
@@ -224,12 +229,66 @@ class Maze:
 
             success_rate = successful_attempts / attempts
             density_vs_nodes.loc[i] = [density, success_rate]
-            density_vs_nodes.to_csv("dfs_success.csv", mode='a', index=False)
-            i += 1"""
+            density_vs_nodes.to_csv("bfs_success.csv", mode='a', index=False)
+            i += 1
 
         print(density_vs_nodes.head())
         density_vs_nodes.plot(x="Density", y="Success Rate")
         plt.show()
+        """
+        """This loop is how we tested for successful attempts"""
+        """density_vs_nodes_a_star = pd.DataFrame(columns=('Density', 'Average Nodes'))
+        total_nodes = 0
+        i = 0
+        attempts = 0
+        n_trials = 0
+
+        for density in np.arange(0.3, 1, 0.1):
+            total_nodes = 0
+            successful_attempts = 0
+            while successful_attempts < 10:
+                print("Successful attempts so far", successful_attempts)
+                self.maze = np.random.choice(
+                    a=[0, 1],
+                    size=(dim, dim),
+                    p=[1 - density, density])
+                self.maze[0][0] = 0
+                self.maze[dim - 1][dim - 1] = 0
+                try:
+                    path, nodes_visited = self.bfs(start, end)
+
+                    if attempts == -1:
+                        print(f"Test {n_trials} has failed multiple times at current density {density} and will not repeat")
+                        n_trials += 1
+                        attempts = 0
+                        continue
+                    if nodes_visited == -1:
+                        print("No path found, rerunning test")
+                        attempts += 1
+                        print(f"Current trial:  {successful_attempts} ,Attempt:  {attempts}  at {density} density")
+                        continue
+                except:
+                    print("An error has occurred, rerunning test")
+                    attempts += 1
+                    continue
+                total_nodes += nodes_visited
+                # attempts = 0
+                This for loop and self.display_maze() print the maze with a solved path
+                for x, y in path:
+                    self.maze[x][y] = 3
+                self.display_maze()
+                successful_attempts += 1
+                print("Density", density)
+                print("Nodes visited", nodes_visited)
+                n_trials += 1
+            average_nodes = total_nodes / successful_attempts
+            density_vs_nodes_a_star.loc[i] = [density, average_nodes]
+            density_vs_nodes_a_star.to_csv("density_vs_nodes_dfs.csv", mode='a', index=False)
+            i += 1
+
+        print(density_vs_nodes_a_star.head())
+        density_vs_nodes_a_star.plot(x="Density", y="Average Nodes")
+        plt.show()"""
 
     def display_maze(self):
         """check if there is fire in the maze"""
@@ -249,16 +308,19 @@ class Maze:
         plt.show()  # display the solved maze
 
     def maze2graph(self):
+        """Converts the maze into a graph for the dfs to solve easier"""
         height = len(self.maze)
         width = len(self.maze[0]) if height else 0
+        """Creates the graph based on the open spots of the maze."""
         graph = {(x, y): [] for y in range(width) for x in range(height) if not self.maze[x][y]}
         for row, col in graph.keys():
-            if row < height - 1 and not self.maze[row + 1][col]:
-                graph[(row, col)].append((row + 1, col))
-                graph[(row + 1, col)].append((row, col))
+            """Checks to see if the nodes in the graph are truly empty, and adds it to graph it it is"""
             if col < width - 1 and not self.maze[row][col + 1]:
                 graph[(row, col)].append((row, col + 1))
                 graph[(row, col + 1)].append((row, col))
+            if row < height - 1 and not self.maze[row + 1][col]:
+                graph[(row, col)].append((row + 1, col))
+                graph[(row + 1, col)].append((row, col))
         return graph
 
     def dfs(self, start, end):
@@ -281,15 +343,19 @@ class Maze:
             # print(path)
             # print(current)
             if current == goal:
+                """If the current node is the end node, we confirm that it has a path 
+                and return the list of visited nodes to show the path"""
                 has_path = True
                 visited.add(current)
                 print("Path Exists? ", has_path)
                 return visited, nodes_visited
             if current in visited:
+                """Helps with back-tracking"""
                 continue
             visited.add(current)
             try:
                 for neighbor in graph[current]:
+                    """Checks if the neighbor is a true neighbor, if it is add it to the stack"""
                     stack.append(neighbor)
             except KeyError:
                 continue
@@ -298,54 +364,60 @@ class Maze:
         return empty_visit, -1
 
     def get_bfs_path(self, path, ans):
+        """Current node is set to the end node to begin path creation"""
         current_node = ans
         end = []
-
-        while current_node != (0, 0):
+        """moves though the nodes that were visted to create the solved path rather than all nodes visited"""
+        while (current_node != (0, 0)):
             end.append(current_node)
             current_node = path[current_node]
-
+        """Adds the starter node to the path then reverses it to reset the queue"""
         end.append((0, 0))
         end.reverse()
-
+        """returns the list of completed path"""
         return end
 
     def bfs(self, start, end):
-
+        """Implementation of BFS"""
+        """ BFS uses queue to search childern and add them to fringe"""
+        """Maze length will allow to find end and the size of the maze"""
         maze_length = self.dim
         maze_sol = (maze_length-1, maze_length-1)
-
+        """The visisted list is used to define the path that finds the solution as well as identify dead ends"""
         visited = [[False for x in range(maze_length)] for y in range(maze_length)]
+        """path is the final path to the solution"""
         path = {}
         total = 0
+        """The original neighbors that the children are made form"""
         neighbors = [(0, -1), (-1, 0), (0, 1), (1, 0)]
         path_exists = False
-
+        """The main queue that is manipulated through the algoritihim"""
         queue = []
-
+        """Nodes_visited is incremented per node visited to calcualte the total nodes"""
         nodes_visited = 0
         queue.append((0,0))
         visited[0][0] = True
 
         nodes_visited = 0
-
+        """While the queue is populated continue running the BFS algorithim"""
         while queue:
+            self.advance_fire_one_step()
             nodes_visited += 1
             total = max(total, len(queue))
-
+            """Current node is the node that is on the front of the queue"""
             current_node = queue.pop()
-
+            """Checking if the the current node is the maze solution"""
             if current_node == maze_sol:
                 path_exists = True
                 break
-
+            """For the child is a neighbor of the current node run the movement loop"""
             for child in neighbors:
-
+                """This is for incrementing and moving the position of the node"""
                 spot = tuple(sum(x) for x in zip(current_node, child))
 
                 x = spot [0]
                 y = spot[1]
-
+                """These if statements are to ensure that the x and y spots are open nodes in the maze and good to move into"""
                 if 0 <= x < self.dim:
 
                     if 0 <= y < self.dim:
@@ -353,18 +425,19 @@ class Maze:
                         if not self.maze[x][y]:
 
                             if not visited[x][y]:
-
+                                """If the node is open then add it to the queue to be visited next"""
                                 queue.append(spot)
                                 path[spot] = current_node
                                 visited[spot[0]][spot[1]] = True
-
+        """If there is no possible paths return the path at the dead end and -1 so the Init is aware there is no path"""
         if not path_exists:
             path = []
             path.append(current_node)
+            """Path exists will show the maze is blocked"""
             path_exists = False
 
             return path[::-1], -1
-
+        """Return the get path method with the solved path and end node so the path is created and not all visited nodes"""
         return self.get_bfs_path(path, maze_sol), nodes_visited
 
     def a_star(self, start, end):
